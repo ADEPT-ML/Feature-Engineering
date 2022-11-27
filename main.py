8dbae8695fda55173288cf529db744a1de941bd0
Benedikt Tobias <61744142+benedikttobias@users.noreply.github.com>
2022-11-27 18:05:36 +0100

    Deactivated specific CORS settings for internal services

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import pandas
 from src import features, schema
 
 from fastapi import FastAPI, Body
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins = ["*"]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 class JSONEncoder(json.JSONEncoder):
